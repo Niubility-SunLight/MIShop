9839d4de0bb96b3cb6621b026e34c78ac2787856
Niubility <nzuondiewyc@gmail.com>
2019-08-26 21:02:33 +0800

    no message

---
+++ src/modules/home/data.js
@@ -0,0 +1,82 @@
+export const tabs= [
+    {
+        name: "推荐",
+        page_type: "home",
+        page_id: 0,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "手机",
+        page_type: "activity",
+        page_id: 8235,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "智能",
+        page_type: "activity",
+        page_id: 10288,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "电视",
+        page_type: "activity",
+        page_id: 11073,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "笔记本",
+        page_type: "activity",
+        page_id: 10874,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "家电",
+        page_type: "activity",
+        page_id: 11078,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "5G专场",
+        page_type: "activity",
+        page_id: 11243,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    },
+    {
+        name: "生活周边",
+        page_type: "tab",
+        page_id: 1861,
+        is_default: false,
+        word_selected_color: "#ed5b00",
+        word_unselected_color: "#747474",
+        bg_img: "",
+        bg_img_webp: ""
+    }
+]
